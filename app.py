df6f2c56a00185f0d865c208756e0a9e88a1f6a0
kiehnema <kiehnema@katharineum.de>
2026-04-20 15:21:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.write("Lade ein Bild einer Pflanze hoch.")
 # ----------------------------
 @st.cache_resource
 def load_tm_model():
-    model = load_model("keras_Model.h5", compile=False)
+    model = load_model("keras_model.h5", compile=False)
     class_names = open("labels.txt", "r").readlines()
     return model, class_names
 
